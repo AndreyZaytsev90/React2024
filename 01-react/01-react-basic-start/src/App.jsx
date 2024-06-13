bd8930e29f40e4a9d0d96374106fce8f43c18bb9
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2024-06-13 20:53:30 +0300

    update file

--- 01-react/01-react-basic-start/src/App.jsx
+++ 01-react/01-react-basic-start/src/App.jsx
@@ -1,12 +1,25 @@
 import Header from "./components/Header.jsx";
+import {WantToLearn} from "../wantToLearn.jsx";
+
+export const technologies = [
+    {name: 'Javascript', other: 'Язык программирования для веб-разработки'},
+    {name: 'React', other: 'Библиотека для веб-разработки'},
+]
 
 
 export default function App() {
+
     return (
         <div>
             <Header/>
             <main>
-                <h1>Hello React!</h1>
+                <section>
+                    <h3>Необходимо выучить</h3>
+                    <ul>
+                        <WantToLearn/>
+                    </ul>
+
+                </section>
             </main>
         </div>
     )
---
+++ 01-react/01-react-basic-start/wantToLearn.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {technologies} from "./src/App.jsx"; // Не забудьте импортировать React
+
+export function WantToLearn() {
+    return (
+        <ul>
+            {technologies.map((item, index) => ( // Добавлен index как key
+                <li key={index}>
+                    <p>
+                        <strong>{item.name}.</strong> {item.other}
+                    </p>
+                </li>
+            ))}
+        </ul>
+    )
+}
